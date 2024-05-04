7c4abe8920f03229b308b528bfc1c479c22725a4
Toni Lingårdsson Luna <toni.lingardsson@gmail.com>
2024-05-05 01:56:44 +0200

    Updated and improved Blockchain routes.

--- controllers/blockchainController.mjs
+++ controllers/blockchainController.mjs
@@ -1,7 +1,7 @@
 import { blockchain } from "../startup.mjs";
-import ErrorResponse from "../utils/ErrorResponse.mjs";
-import FileHandler from "../utils/FileHandler.mjs";
 import ServerResponse from "../utils/ServerResponse.mjs";
+import FileHandler from "../utils/FileHandler.mjs";
+import ErrorResponse from "../utils/ErrorResponse.mjs";
 
 const getBlockchain = (req, res, next) => {
     res
@@ -24,15 +24,20 @@ const getBlockByIndex = (req, res, next) => {
             new ErrorResponse(`Cannot find block with index ${index}`, 404)
         );
     }
-    res.json({ data: block });
+    res
+        .status(200)
+        .json(new ServerResponse({ status: 200, data: block }))
 };
 
 const mineBlock = (req, res, next) => {
     const block = blockchain.proofOfWork(req.body);
 
-    new FileHandler('data', 'blockchain.json').write(blockchain);
+    new FileHandler('data',
+        `blockchain-${process.argv[2]}.json`).write(blockchain);
 
-    res.json({ data: block });
+    res
+        .status(201)
+        .json(new ServerResponse({ status: 201, data: block }))
 };
 
 const synchronizeChain = (req, res, next) => { };
